799b7b712b82576ab71c31eaf5f648efe0876253
venika-sem <99354917+venika-sem@users.noreply.github.com>
2022-04-05 17:08:12 +0000

    changed script.js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function calculate() {
   const radiusOfSphere = parseFloat(document.getElementById('radius-of-sphere').value)
 
    // process
-  const volume = (4 / 3) * Math.PI * radiusOfSphere * radiusOfSphere * radiusOfSphere
+  const volume = (4 / 3) * Math.PI * Math.Pow(radiusOfSphere, 3)
 
   // output
   document.getElementById('volume').innerHTML = '<p>The volume is: ' + volume.toFixed(2) + ' cm³</p>'
